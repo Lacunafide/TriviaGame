f3daffee983441ae7bfda8eaf1e6eb05bf492bf8
Michael Martin <Lacunafide@gmail.com>
2019-04-02 19:44:49 -0400

    finishing touch
    last changes made

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -82,3 +82,86 @@ var questions = [{
 
 //---------------------------------------------------------
 
+var labels = ["first", "second", "third", "forth"];
+var startGame = $("#startBtn").on('click', function() {
+$(this).parent().hide();
+$('.container').show();
+countdown(60);
+questionDisplay();
+});
+
+var questionDisplay = function() {
+$(".questions :not('#submit')").empty();
+for (var j = 0; j < 10; j++) {
+$('.questions').prepend('<div class="' + questions[j].name + '"></div>');
+$(questions[j].divClass).append('<div class ="ques-title">' + questions[j].ques + '</div>');
+for (var i = 0; i <= 3; i++) {
+    $(questions[j].divClass).append('<input type="radio"  name="' + questions[j].name + '" value="' + questions[j].ans[i] + '"/> <label for="' + labels[i] + '">' + questions[j].ans[i] + '</label> ');
+}
+$('.questions').prepend('<hr />');
+}
+}
+
+
+var countdown = function(seconds) {
+
+var timer = setInterval(function() {
+seconds = seconds - 1;
+$("#timeLeft").html(seconds);
+
+if (seconds <= 0) {
+    $('.container').fadeOut(500);
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
+    var unAnswered = 0;
+        
+        for (var i = 0; i < 10; i++) {
+
+        if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+
+            correctAnswers++;
+            console.log("this is correct! number:" + i)
+        } else {
+            wrongAnswers++;
+            console.log("this is wrong! number:" + i)
+        };
+    }
+    $('#correctTimeUp').append(correctAnswers);
+    $('#wrongTimeUp').append(wrongAnswers);
+    $('#timeUp').fadeIn(1000).show();
+
+    clearInterval(timer);
+    return;
+}
+}, 1000);
+
+$('#submit').on('click', function() {
+clearInterval(timer);
+})
+};
+
+
+var gradeQuiz = $('#submit').on('click', function() {
+
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var unAnswered = 0;
+
+for (var i = 0; i < 10; i++) {
+
+if ($('input:radio[name="' + questions[i].name + '"]:checked').val() === questions[i].correct) {
+
+    correctAnswers++;
+} else {
+    wrongAnswers++;
+};
+};
+
+
+countdown();
+$('.container').fadeOut(500);
+$('#answers').show();
+$('#correctAnswers').append(correctAnswers);
+$('#wrongAnswers').append(wrongAnswers);
+
+});
